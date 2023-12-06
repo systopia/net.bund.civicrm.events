502aed0d256305a450ba80dcc98f5194fc48ba28
Charlotte Jana <jana@systopia.de>
2023-12-06 10:16:42 +0100

    add automatic messages for overlapping event registrations

--- CRM/Events/Form/Settings.php
+++ CRM/Events/Form/Settings.php
@@ -14,189 +14,217 @@
 | written permission from the original author(s).        |
 +--------------------------------------------------------*/
 
-
+use Civi\Api4\OptionValue;
 use CRM_Events_ExtensionUtil as E;
 
 /**
  * General settings regarding BUND events
  */
-class CRM_Events_Form_Settings extends CRM_Core_Form
-{
+class CRM_Events_Form_Settings extends CRM_Core_Form {
 
-    const SETTINGS = [
+  const SETTINGS = [
+    'bund_event_types',
+    'bund_event_relationship_types',
+    'bund_event_recipient_ids',
+    'bund_event_from_email_address',
+  ];
+
+  public function buildQuickForm() {
+    $this->setTitle(E::ts('BUND Event Configuration'));
+
+    $this->add(
+        'select',
         'bund_event_types',
+        E::ts('Event Types (for Volunteers)'),
+        $this->getEventTypes(),
+        FALSE,
+        ['class' => 'crm-select2', 'multiple' => 'multiple', 'placeholder' => E::ts('all')]
+    );
+    $this->add(
+        'select',
         'bund_event_relationship_types',
-    ];
+        E::ts('Relationship Types'),
+        $this->getEventRelationshipTypes(),
+        FALSE,
+        ['class' => 'crm-select2', 'multiple' => 'multiple', 'placeholder' => E::ts('disabled')]
+    );
+    $this->add(
+        'text',
+        'bund_event_recipient_ids',
+        E::ts("Contacts to receive notifications (comma separated contact ids)"),
+        FALSE,
+        FALSE,
+        ['class' => 'crm-select2', 'multiple' => 'multiple', 'placeholder' => E::ts('none')]
+    );
+    $this->add(
+        'select',
+        'bund_event_from_email_address',
+        E::ts('From email address'),
+        $this->getFromEmailAddresses(),
+        TRUE,
+        ['class' => 'crm-select2', 'placeholder' => E::ts('none')]
+    );
 
-    public function buildQuickForm()
-    {
-        $this->setTitle(E::ts("BUND Event Configuration"));
-
-        $this->add(
-            'select',
-            'bund_event_types',
-            E::ts("Event Types (for Volunteers)"),
-            $this->getEventTypes(),
-            false,
-            ['class' => 'crm-select2', 'multiple' => 'multiple', 'placeholder' => E::ts("all")]
-        );
-        $this->add(
-            'select',
-            'bund_event_relationship_types',
-            E::ts("Relationship Types"),
-            $this->getEventRelationshipTypes(),
-            false,
-            ['class' => 'crm-select2', 'multiple' => 'multiple', 'placeholder' => E::ts("disabled")]
-        );
-
-        $this->addButtons(
+    $this->addButtons(
+        [
             [
-                [
-                    'type' => 'submit',
-                    'name' => E::ts('Save'),
-                    'isDefault' => true,
-                ],
-            ]
-        );
+              'type' => 'submit',
+              'name' => E::ts('Save'),
+              'isDefault' => TRUE,
+            ],
+        ]
+    );
 
-        // set current values
-        foreach (self::SETTINGS as $field_name) {
-            $this->setDefaults([$field_name => Civi::settings()->get($field_name)]);
-        }
-
-        parent::buildQuickForm();
+    // set current values
+    foreach (self::SETTINGS as $field_name) {
+      $this->setDefaults([$field_name => Civi::settings()->get($field_name)]);
     }
 
-    public function postProcess()
-    {
-        $values = $this->exportValues();
+    parent::buildQuickForm();
+  }
 
-        // store settings
-        foreach (self::SETTINGS as $field_name) {
-            Civi::settings()->set($field_name, CRM_Utils_Array::value($field_name, $values));
-        }
+  public function postProcess() {
+    $values = $this->exportValues();
 
-        // add user message
-        CRM_Core_Session::setStatus(E::ts("Configuration Saved"), E::ts("Saved"), 'info');
-
-        parent::postProcess();
+    // store settings
+    foreach (self::SETTINGS as $field_name) {
+      Civi::settings()->set($field_name, CRM_Utils_Array::value($field_name, $values));
     }
 
-    /**
-     * Get a list of event types
-     *
-     * @return array
-     *   event type ID => name
-     */
-    protected function getEventTypes()
-    {
-        $event_types = ['' => E::ts("all")];
-        $query = civicrm_api3(
-            'OptionValue',
-            'get',
-            [
-                'option_group_id' => 'event_type',
-                'option.limit'    => 0,
-                'is_active'       => 1,
-                'return'          => 'value,label',
-            ]
-        );
-        foreach ($query['values'] as $type) {
-            $event_types[$type['value']] = $type['label'];
-        }
+    // add user message
+    CRM_Core_Session::setStatus(E::ts('Configuration Saved'), E::ts('Saved'), 'info');
 
-        return $event_types;
+    parent::postProcess();
+  }
+
+  /**
+   * Get a list of event types
+   *
+   * @return array
+   *   event type ID => name
+   */
+  public static function getEventTypes() {
+    $event_types = ['' => E::ts('all')];
+    $query = civicrm_api3(
+        'OptionValue',
+        'get',
+        [
+          'option_group_id' => 'event_type',
+          'option.limit'    => 0,
+          'is_active'       => 1,
+          'return'          => 'value,label',
+        ]
+    );
+    foreach ($query['values'] as $type) {
+      $event_types[$type['value']] = $type['label'];
     }
 
-    /**
-     * Get a list of eligible relationship types
-     *
-     * @return array
-     *   relationship key => label
-     */
-    protected function getEventRelationshipTypes()
-    {
-        $relationship_types = [];
+    return $event_types;
+  }
 
-        // get one direction
-        $type_ids_listed = [0];
-        $query = civicrm_api3(
-            'RelationshipType',
-            'get',
-            [
-                'option.limit'    => 0,
-                'contact_type_a'  => 'Individual',
-                'is_active'       => 1,
-                'return'          => 'id,name_a_b,name_b_a',
-            ]
-        );
-        foreach ($query['values'] as $type) {
-            $type_ids_listed[] = $type['id'];
-            $relationship_types["{$type['id']}a"] = $type['name_a_b'];
-        }
+  /**
+   * Get a list of eligible relationship types
+   *
+   * @return array
+   *   relationship key => label
+   */
+  public static function getEventRelationshipTypes() {
+    $relationship_types = [];
 
-        // get one direction
-        $query = civicrm_api3(
-            'RelationshipType',
-            'get',
-            [
-                'option.limit'    => 0,
-                'contact_type_b'  => 'Individual',
-                'id'              => ['NOT IN' => $type_ids_listed],
-                'is_active'       => 1,
-                'return'          => 'id,name_a_b,name_b_a',
-            ]
-        );
-        foreach ($query['values'] as $type) {
-            $relationship_types["{$type['id']}b"] = $type['name_b_a'];
-        }
-
-        return $relationship_types;
+    // get one direction
+    $type_ids_listed = [0];
+    $query = civicrm_api3(
+        'RelationshipType',
+        'get',
+        [
+          'option.limit'    => 0,
+          'contact_type_a'  => 'Individual',
+          'is_active'       => 1,
+          'return'          => 'id,name_a_b,name_b_a',
+        ]
+    );
+    foreach ($query['values'] as $type) {
+      $type_ids_listed[] = $type['id'];
+      $relationship_types["{$type['id']}a"] = $type['name_a_b'];
     }
 
-    /**
-     * Get a list of eligible custom fields
-     *
-     * @return array
-     *   event type ID => name
-     */
-    protected function getEligibleContingentFields()
-    {
-        // first: get all custom groups with contacts
-        $custom_groups = civicrm_api3(
-            'CustomGroup',
-            'get',
-            [
-                'option.limit'    => 0,
-                'extends'         => ['IN' => ['Contact', 'Individual']],
-                'is_active'       => 1,
-                'sequential'      => 0,
-                'return'          => 'id,title',
-            ]
-        );
-        $eligible_custom_group_ids = array_keys($custom_groups['values']);
-
-        // then: get all eligible fields
-        $eligible_fields = [];
-        $query = civicrm_api3(
-            'CustomField',
-            'get',
-            [
-                'option.limit'    => 0,
-                'custom_group_id' => ['IN' => $eligible_custom_group_ids],
-                'contact_type_b'  => 'Individual',
-                'data_type'       => 'Int',
-                'is_active'       => 1,
-                'return'          => 'id,label,custom_group_id',
-            ]
-        );
-        foreach ($query['values'] as $field) {
-            $eligible_fields[$field['id']] = E::ts("%1 (%2)", [
-                1 => $field['label'],
-                2 => $custom_groups['values'][$field['custom_group_id']]['title']
-            ]);
-        }
-
-        return $eligible_fields;
+    // get one direction
+    $query = civicrm_api3(
+        'RelationshipType',
+        'get',
+        [
+          'option.limit'    => 0,
+          'contact_type_b'  => 'Individual',
+          'id'              => ['NOT IN' => $type_ids_listed],
+          'is_active'       => 1,
+          'return'          => 'id,name_a_b,name_b_a',
+        ]
+    );
+    foreach ($query['values'] as $type) {
+      $relationship_types["{$type['id']}b"] = $type['name_b_a'];
     }
+
+    return $relationship_types;
+  }
+
+  /**
+   * Get a list of configured from email addresses
+   *
+   * @return array
+   */
+  public static function getFromEmailAddresses() {
+    $from_addresses = OptionValue::get(FALSE)
+      ->addWhere('option_group_id:name', '=', 'from_email_address')
+      ->execute()
+      ->indexBy('name')
+      ->column('label');
+
+    return $from_addresses;
+  }
+
+  /**
+   * Get a list of eligible custom fields
+   *
+   * @return array
+   *   event type ID => name
+   */
+  protected function getEligibleContingentFields() {
+    // first: get all custom groups with contacts
+    $custom_groups = civicrm_api3(
+        'CustomGroup',
+        'get',
+        [
+          'option.limit'    => 0,
+          'extends'         => ['IN' => ['Contact', 'Individual']],
+          'is_active'       => 1,
+          'sequential'      => 0,
+          'return'          => 'id,title',
+        ]
+    );
+    $eligible_custom_group_ids = array_keys($custom_groups['values']);
+
+    // then: get all eligible fields
+    $eligible_fields = [];
+    $query = civicrm_api3(
+        'CustomField',
+        'get',
+        [
+          'option.limit'    => 0,
+          'custom_group_id' => ['IN' => $eligible_custom_group_ids],
+          'contact_type_b'  => 'Individual',
+          'data_type'       => 'Int',
+          'is_active'       => 1,
+          'return'          => 'id,label,custom_group_id',
+        ]
+    );
+    foreach ($query['values'] as $field) {
+      $eligible_fields[$field['id']] = E::ts('%1 (%2)', [
+        1 => $field['label'],
+        2 => $custom_groups['values'][$field['custom_group_id']]['title'],
+      ]);
+    }
+
+    return $eligible_fields;
+  }
+
 }
--- CRM/Events/Upgrader.php
+++ CRM/Events/Upgrader.php
@@ -1,8 +1,8 @@
 <?php
 /*-------------------------------------------------------+
 | BUND Event Customisations                              |
-| Copyright (C) 2020 SYSTOPIA                            |
-| Author: B. Endres (endres@systopia.de)                 |
+| Copyright (C) 2023 SYSTOPIA                            |
+| Author: C. Jana (jana@systopia.de)                     |
 | http://www.systopia.de/                                |
 +--------------------------------------------------------+
 | This program is released as free software under the    |
@@ -14,19 +14,53 @@
 | written permission from the original author(s).        |
 +--------------------------------------------------------*/
 
+declare(strict_types = 1);
 
+use Civi\Api4\MessageTemplate;
 use CRM_Events_ExtensionUtil as E;
 
 /**
  * Collection of upgrade steps.
  */
-class CRM_Events_Upgrader extends CRM_Extension_Upgrader_Base
-{
+class CRM_Events_Upgrader extends CRM_Extension_Upgrader_Base {
 
-    /**
-     * Installer
-     */
-    public function install()
-    {
-    }
+  /**
+   * Installer
+   */
+  public function install() {
+  }
+
+  public function enable() {
+
+    $overlap_text = file_get_contents(E::path('templates/Events/MessageTemplates/overlapping_events.txt'));
+
+    $overlap_html = file_get_contents(E::path('templates/Events/MessageTemplates/overlapping_events.html'));
+
+    $overlap_tpl = [
+      'workflow_name' => 'participant_overlap_bund',
+      'msg_title' => 'Events - Overlap notification for BUND events',
+      'msg_subject' => 'Parallelbuchung BFD-Seminare',
+      'msg_text' => $overlap_text,
+      'msg_html' => $overlap_html,
+    ];
+
+    // Create a "reserved" template. This is a pristine copy provided for reference.
+    civicrm_api4('MessageTemplate', 'create',
+            [
+              'values' => $overlap_tpl + ['is_reserved' => 1, 'is_default' => 0],
+            ]);
+
+    // Create a default template. This is live. The administrator may edit/customize.
+    civicrm_api4('MessageTemplate', 'create',
+            [
+              'values' => $overlap_tpl + ['is_reserved' => 0, 'is_default' => 1],
+            ]);
+  }
+
+  public function disable() {
+    MessageTemplate::delete(TRUE)
+      ->addWhere('workflow_name', '=', 'participant_overlap_bund')
+      ->execute();
+  }
+
 }
